4146b744f15d13c48f56a30dc734e8a7f80f37fa
DuskyShelf <dupsdale@gmail.com>
2024-07-23 11:41:50 +0100

    adds createInvoice and fixes linting

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,7 +7,6 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-
 export default async function Page({
   searchParams,
 }: {
@@ -18,7 +17,7 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  
+
   const totalPages = await fetchInvoicesPages(query);
 
   return (
